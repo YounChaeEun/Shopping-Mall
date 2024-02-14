e459e18902199871703b78ce41f8fe3722a6c193
YounCheEun <amy4021@naver.com>
2024-02-14 10:56:23 +0900

    Fix: BaseEntity 안에 import 에러 수정

--- src/main/java/com/example/shoppingmall_comp/domain/BaseEntity.java
+++ src/main/java/com/example/shoppingmall_comp/domain/BaseEntity.java
@@ -1,8 +1,5 @@
 package com.example.shoppingmall_comp.domain;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,6 +7,9 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
