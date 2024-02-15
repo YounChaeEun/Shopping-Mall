c8e3ac835b2b65600aaccf3c6786db40b398c6c3
YounCheEun <amy4021@naver.com>
2024-02-15 11:19:53 +0900

    Add: 리프레시 토큰 엔티티 추가

---
+++ src/main/java/com/example/shoppingmall_comp/domain/members/entity/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.shoppingmall_comp.domain.members.entity;
+
+import com.example.shoppingmall_comp.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_token_id")
+    private long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private String refreshToken;
+
+    @Builder
+    public RefreshToken(Member member, String refreshToken) {
+        this.member = member;
+        this.refreshToken = refreshToken;
+    }
+}
