57b565b2435f3099d3617598dfa36cdf547d242a
YounCheEun <amy4021@naver.com>
2024-02-15 15:10:53 +0900

    Feat: 토큰 api에 swagger 코드 추가

--- src/main/java/com/example/shoppingmall_comp/domain/members/controller/TokenController.java
+++ src/main/java/com/example/shoppingmall_comp/domain/members/controller/TokenController.java
@@ -3,6 +3,7 @@ package com.example.shoppingmall_comp.domain.members.controller;
 import com.example.shoppingmall_comp.domain.members.dto.CreateAccessTokenReponse;
 import com.example.shoppingmall_comp.domain.members.dto.CreateAccessTokenRequest;
 import com.example.shoppingmall_comp.domain.members.service.serviceImpl.TokenServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +13,14 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "토큰 관련 api", description = "토큰 관련 api입니다.")
 public class TokenController {
 
     private final TokenServiceImpl tokenService;
 
     @PostMapping("/token")
     @ResponseStatus(HttpStatus.CREATED)
+    @Tag(name = "엑세스 토큰 재발급api", description = "리프레시 토큰으로 새 엑세스 토큰 발급해주는 api입니다.")
     public CreateAccessTokenReponse createNewAccessToken(@RequestBody @Valid CreateAccessTokenRequest request) {
         return tokenService.createNewAccessToken(request);
     }
--- src/main/java/com/example/shoppingmall_comp/domain/members/dto/CreateAccessTokenReponse.java
+++ src/main/java/com/example/shoppingmall_comp/domain/members/dto/CreateAccessTokenReponse.java
@@ -1,6 +1,9 @@
 package com.example.shoppingmall_comp.domain.members.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateAccessTokenReponse(
+        @Schema(description = "사용자의 새 엑세스 토큰", example = "hfdhfskjhfskjhdkjshkdj")
         String accessToken
 ) {
 }
