d92e199d92e249f83f06c56dd51a6e93664d39f6
daye <jasmineldy@naver.com>
2024-02-18 12:14:19 +0900

    Add: ItemOptionRepository에 메소드 추가

--- src/main/java/com/example/shoppingmall_comp/domain/items/repository/ItemOptionRepository.java
+++ src/main/java/com/example/shoppingmall_comp/domain/items/repository/ItemOptionRepository.java
@@ -5,6 +5,9 @@ import com.example.shoppingmall_comp.domain.items.entity.ItemOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemOptionRepository extends JpaRepository<ItemOption, Long> {
+    List<ItemOption> findByItem(Item item);
 }
