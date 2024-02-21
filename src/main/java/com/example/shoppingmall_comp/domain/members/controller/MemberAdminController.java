7d5c4a81c9a73881aed1de6a8f8e09e7fbca862d
YounChaeEun <amy4021186@gmail.com>
2024-02-21 21:11:03 +0900

    Feat: swagger 코드 추가

--- src/main/java/com/example/shoppingmall_comp/domain/members/controller/MemberAdminController.java
+++ src/main/java/com/example/shoppingmall_comp/domain/members/controller/MemberAdminController.java
@@ -2,6 +2,8 @@ package com.example.shoppingmall_comp.domain.members.controller;
 
 import com.example.shoppingmall_comp.domain.members.dto.MemberSignUpResponse;
 import com.example.shoppingmall_comp.domain.members.service.impl.MemberServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@Tag(name = "관리자의 멤버 관련 api", description = "관리자의 멤버 관련 api들입니다.")
 public class MemberAdminController {
 
     private final MemberServiceImpl memberService;
@@ -21,6 +24,7 @@ public class MemberAdminController {
     // 회원 전체 조회
     @GetMapping("/members")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "관리자의 회원 전체 조회 api", description = "관리자가 모든 회원들을 조회하는 api 입니다.")
     public List<MemberSignUpResponse> getAllMembers() {
         return memberService.getAll();
     }
--- src/main/java/com/example/shoppingmall_comp/domain/members/controller/MemberController.java
+++ src/main/java/com/example/shoppingmall_comp/domain/members/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.shoppingmall_comp.domain.members.controller;
 
 import com.example.shoppingmall_comp.domain.members.dto.MemberSignUpResponse;
 import com.example.shoppingmall_comp.domain.members.service.impl.MemberServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,17 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "멤버 관련 api", description = "회원 정보 조회, 삭제 api들입니다.")
 public class MemberController {
 
     private final MemberServiceImpl memberService;
     @GetMapping("/members")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "회원 상세 조회 api", description = "사용자가 자신의 정보를 조회하는 api 입니다.")
     public MemberSignUpResponse getOneMember(@AuthenticationPrincipal User user) {
         return memberService.getOne(user);
     }
 
     @DeleteMapping("/members")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "회원 탈퇴 api", description = "일반 사용자가 탈퇴하는 api 입니다.")
     public void deleteUser(@AuthenticationPrincipal User user) {
         memberService.deleteUser(user);
     }
