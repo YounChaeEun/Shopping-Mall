e9f84c5efa9dcd71b52eafe529d6b6802fe2f5bf
daye <jasmineldy@naver.com>
2024-02-23 13:37:03 +0900

    Fix: MemberServiceImpl 위치 수정

--- src/main/java/com/example/shoppingmall_comp/domain/orders/repository/OrderItemRepository.java
+++ src/main/java/com/example/shoppingmall_comp/domain/orders/repository/OrderItemRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrder(Order order); //특정 주문에 해당하는 모든 주문상품 리스트
     List<OrderItem> findAllByOrder(Order order);
+
 }
