e7d6f7f77a7053e837bc754c905d7ddd4c76e088
daye <jasmineldy@naver.com>
2024-03-03 21:27:17 +0900

    Fix: ItemResponse 수정, 상품 이미지 경로 Swagger example 변경

--- src/main/java/com/example/shoppingmall_comp/domain/items/dto/ItemResponse.java
+++ src/main/java/com/example/shoppingmall_comp/domain/items/dto/ItemResponse.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
-@Schema(description = "상품 응답 DTO")
+@Schema(description = "상품 상세 조회 응답 DTO")
 public record ItemResponse (
 
         @Schema(description = "상품 id", example = "1")
@@ -20,9 +20,6 @@ public record ItemResponse (
         @Schema(description = "상품 가격", example = "879000")
         int price,
 
-        @Schema(description = "상품 수량", example = "1000")
-        int count,
-
         @Schema(description = "상품 옵션", example = "{색상: WHITE}")
         List<Option> optionValue,
 
@@ -32,7 +29,7 @@ public record ItemResponse (
         @Schema(description = "상품 상세 설명", example = "가볍고 화질이 선명해요.")
         String description,
 
-        @Schema(description = "상품 이미지 url들", example = "https://dachaebucket.s3.ap-northeast-2.amazonaws.com/123.jpg")
+        @Schema(description = "상품 이미지 url 리스트", example = "[\"https://dachaebucket.s3.ap-northeast-2.amazonaws.com/123.jpg\"]")
         List<String> imgUrls
 
 ) {
