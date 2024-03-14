3a234337c76af7d41ffd9a6251bf89402cff5759
YounChaeEun <amy4021186@gmail.com>
2024-03-14 21:37:18 +0900

    Test: 상품의 리뷰 전체 조회 성공 테스트

--- src/test/java/com/example/shoppingmall_comp/domain/members/service/implement/ReviewServiceImplTest.java
+++ src/test/java/com/example/shoppingmall_comp/domain/members/service/implement/ReviewServiceImplTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.ArrayList;
@@ -23,13 +26,15 @@ class ReviewServiceImplTest {
     @Autowired
     ReviewRepository reviewRepository;
     private User user;
+    private Pageable pageable;
 
     @BeforeEach
     void setUp() {
         this.user = new User("amy1234@naver.com", "Amy4021*", new ArrayList<>()); // username은 디비에 잇는 거 쓰기
+        this.pageable = PageRequest.of(0, 15, Sort.Direction.DESC, "reviewId");
     }
 
-    @DisplayName("리뷰 생성 테스트")
+    @DisplayName("리뷰 생성 성공 테스트")
     @Test
     void create() {
         // given
@@ -44,7 +49,7 @@ class ReviewServiceImplTest {
         assertThat(response.star()).isEqualTo(3);
     }
 
-    @DisplayName("리뷰 수정 테스트")
+    @DisplayName("리뷰 수정 성공 테스트")
     @Test
     void update() {
         // given
@@ -63,7 +68,7 @@ class ReviewServiceImplTest {
         assertThat(review.getStar()).isEqualTo(5);
     }
 
-    @DisplayName("리뷰 삭제 테스트")
+    @DisplayName("리뷰 삭제 성공 테스트")
     @Test
     void delete() {
         // given
@@ -76,5 +81,22 @@ class ReviewServiceImplTest {
         // then
         List<Review> reviews = reviewRepository.findAll();
         assertThat(reviews.size()).isEqualTo(7);
+    }
+
+    @DisplayName("상품의 리뷰 전체 조회 성공 테스트")
+    @Test
+    void getAllByItem() {
+        // given -> 아이템 여기서 하나 만들어주고 그것에 대한 리뷰 만들어주고 개수 확인해야하나?
+
+        // when
+        var response = reviewService.getAllByItem(1L, pageable);
+
+        // then
+        assertThat(response.responseList().size()).isEqualTo(7);
+        // 굳이 안해도 될 것 같긴 하지만.. pageable 검사 -> 해줘야 하나?
+        assertThat(response.currentPageSize()).isEqualTo(15);
+        assertThat(response.pageNumber()).isEqualTo(0);
+        assertThat(response.totalCount()).isEqualTo(7);
+        assertThat(response.totalPage()).isEqualTo(1);
     }
 }
