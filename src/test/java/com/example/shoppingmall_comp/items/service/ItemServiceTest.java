da94fc9b00f69e197e8a25a75255a47bfe31679c
YounChaeEun <amy4021186@gmail.com>
2024-03-22 17:40:30 +0900

    Test: 상품 삭제 성공 테스트 추가

--- src/test/java/com/example/shoppingmall_comp/items/service/ItemServiceTest.java
+++ src/test/java/com/example/shoppingmall_comp/items/service/ItemServiceTest.java
@@ -1,12 +1,14 @@
 package com.example.shoppingmall_comp.items.service;
 
 import com.example.shoppingmall_comp.domain.items.dto.ItemRequest;
-import com.example.shoppingmall_comp.domain.items.entity.Category;
+import com.example.shoppingmall_comp.domain.items.entity.*;
 import com.example.shoppingmall_comp.domain.items.repository.CategoryRepository;
 import com.example.shoppingmall_comp.domain.items.repository.ItemImageRepository;
 import com.example.shoppingmall_comp.domain.items.repository.ItemOptionRepository;
 import com.example.shoppingmall_comp.domain.items.repository.ItemRepository;
 import com.example.shoppingmall_comp.domain.items.service.ItemService;
+import com.example.shoppingmall_comp.domain.members.entity.Member;
+import com.example.shoppingmall_comp.domain.members.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,22 +39,33 @@ public class ItemServiceTest {
     ItemOptionRepository itemOptionRepository;
     @Autowired
     ItemImageRepository itemImageRepository;
-
     @Autowired
     CategoryRepository categoryRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
 //    @PersistenceContext
 //    EntityManager em;
 
     private User user;
     private Category category;
+    private Member member;
+    private ItemOption itemOption;
 
     @BeforeEach
     void setup() {
         this.user = new User("amy12345seller@naver.com", "Amy4021*", new ArrayList<>());
+        this.member = memberRepository.findByEmail(this.user.getUsername()).orElseThrow();
         this.category = categoryRepository.save(Category.builder()
                 .categoryName("test category")
                 .build());
+        List<ItemOption.Option> options = new ArrayList<>();
+        options.add(new ItemOption.Option("색상", "빨강"));
+        options.add(new ItemOption.Option("사이즈", "large"));
+        this.itemOption = itemOptionRepository.save(ItemOption.builder()
+                .optionValues(options)
+                .build());
+        //this.category = createCategory();
     }
 
     @DisplayName("상품 생성 성공 테스트")
@@ -75,6 +90,39 @@ public class ItemServiceTest {
         var foundOptions = item.getItemOption().getOptionValues();
         assertThat(foundOptions.isEmpty()).isFalse();
         assertThat(foundOptions.size()).isEqualTo(2);
+    }
+
+    @DisplayName("상품 삭제 성공 테스트")
+    @Test
+    void delete() {
+        // given
+        Item item = itemRepository.save(Item.builder()
+                .itemName("test item name")
+                .itemPrice(10000)
+                .itemDetail("test item detail")
+                .count(10000)
+                .itemState(ItemState.ON_SALE)
+                .category(this.category)
+                .itemOption(this.itemOption)
+                .member(this.member)
+                .build());
+
+        itemImageRepository.save(ItemImage.builder()
+                .imageUrl("image url")
+                .item(item)
+                .build());
+
+        // when
+        itemService.delete(item.getItemId(), user);
+
+        // then
+        var items = itemRepository.findAll();
+        assertThat(items.size()).isEqualTo(5);
+
+        var foundImages = itemImageRepository.findByItem(item); // 질문: 이게 맞는지? 디비에서는 삭제되었지만 자바의 메모리에서는 여전히 남아있어서 이 로직을 수행할 수 있다고 하는데..
+        assertThat(foundImages).isEmpty();
+
+        // 옵션 부분
     }
 
     // 추후의 실패 테스트까지 고려해서 setup에 두지 않고 따로 메서드로 뺌, setup은 성공,실패 테스트 모두에 사용 가능한 것만 넣음
