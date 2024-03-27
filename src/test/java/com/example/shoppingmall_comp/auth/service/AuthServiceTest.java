1e2947003753a1bed7ae4f24f6c59650bac9aaa5
YounChaeEun <amy4021186@gmail.com>
2024-03-27 17:54:41 +0900

    Test: 로그인 성공 테스트에서 멤버 객체 생성한 후 테스트하는 걸로 수정

--- src/test/java/com/example/shoppingmall_comp/auth/service/AuthServiceTest.java
+++ src/test/java/com/example/shoppingmall_comp/auth/service/AuthServiceTest.java
@@ -1,9 +1,9 @@
 package com.example.shoppingmall_comp.auth.service;
 
 import com.example.shoppingmall_comp.domain.members.dto.MemberSignInRequest;
-import com.example.shoppingmall_comp.domain.members.dto.MemberSignInResponse;
 import com.example.shoppingmall_comp.domain.members.dto.MemberSignUpRequest;
 import com.example.shoppingmall_comp.domain.members.entity.Member;
+import com.example.shoppingmall_comp.domain.members.entity.Role;
 import com.example.shoppingmall_comp.domain.members.entity.RoleName;
 import com.example.shoppingmall_comp.domain.members.repository.MemberRepository;
 import com.example.shoppingmall_comp.domain.members.repository.RefreshTokenRepository;
@@ -12,9 +12,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -26,34 +27,42 @@ public class AuthServiceTest {
     MemberRepository memberRepository;
     @Autowired
     RefreshTokenRepository refreshTokenRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @DisplayName("회원가입 성공 테스트")
     @Test
     void saveMember() {
         // given
-        var request = new MemberSignUpRequest("amy402123@naver.com", "Amy4021*", RoleName.USER);
+        var request = new MemberSignUpRequest("amy11234@naver.com", "Amy4021!", RoleName.USER);
 
         // when
         authService.saveMember(request);
 
         // then
-        var member = memberRepository.findByEmail(request.email()).get();
-        assertThat(member.getEmail()).isEqualTo("amy402123@naver.com");
+        var member = memberRepository.findByEmail(request.email()).orElseThrow();
+        assertThat(member.getEmail()).isEqualTo("amy11234@naver.com");
         assertThat(member.getRole().getRoleName()).isEqualTo(RoleName.USER);
-        // 질문: 비밀번호는 안해도 되나요?
     }
 
     @DisplayName("로그인 성공 테스트")
     @Test
     void signin() {
         // given
-        var request = new MemberSignInRequest("amy12345seller@naver.com", "Amy4021*");
+        memberRepository.save(Member.builder()
+                .email("amy11234@naver.com")
+                .password(bCryptPasswordEncoder.encode("Amy4021!"))
+                .role(Role.builder()
+                        .roleName(RoleName.USER)
+                        .build())
+                .build());
+        var request = new MemberSignInRequest("amy11234@naver.com", "Amy4021!");
 
         // when
         var response = authService.signIn(request);
 
         // then
-        var member = memberRepository.findByEmail(request.email()).get();
+        var member = memberRepository.findByEmail(request.email()).orElseThrow();
         var refreshToken = refreshTokenRepository.findByMember(member);
         assertThat(refreshToken.isPresent()).isTrue();
         assertThat(refreshToken.get().getRefreshToken()).isEqualTo(response.refreshToken());
